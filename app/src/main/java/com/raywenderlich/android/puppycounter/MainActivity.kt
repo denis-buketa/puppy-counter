a9df0f7e575c4f5734ac4a7cfef692ca6072cdea
dbuketa <denis.buketa@gmail.com>
2021-02-16 20:06:19 +0100

    Opening Share Activity
    - Added menu button to Main Activity
    - Added logic to open Share Activity
    - Added landscape layout for Share Activity

--- app/src/main/java/com/raywenderlich/android/puppycounter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/puppycounter/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.raywenderlich.android.puppycounter
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 /*
@@ -47,5 +51,26 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    val inflater: MenuInflater = menuInflater
+    inflater.inflate(R.menu.menu_activity_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.shareAction -> {
+        startShareActivity()
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
+  private fun startShareActivity() {
+    val shareActivityIntent = Intent(this, ShareActivity::class.java)
+    startActivity(shareActivityIntent)
   }
 }
